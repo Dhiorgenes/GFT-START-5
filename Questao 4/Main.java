fac19da407344721cf260ca52dbbc081e93fd3d3
Dhiorgenes Francisco <dhiorgenessantana@gmail.com>
2022-06-28 17:43:57 -0300

    Add files via upload
    Enviando Questão 4

--- Questao 4/Main.java
+++ Questao 4/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+     Scanner leitor = new Scanner(System.in);
+     int valor;
+     
+     do{
+        System.out.println("Digite um número entre 3 e 999: ");
+        valor = leitor.nextInt();
+     }while(valor < 3 || valor > 999);
+
+     if(valor % 2 == 0 || valor % 3 == 0){
+        System.out.println("O número não é primo.");
+     }else{
+        System.out.println("O número é primo.");
+     }
+
     }
 }
